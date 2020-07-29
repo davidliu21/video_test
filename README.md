daa001c89db1be8ac20975843be7fc59b21cdaff
hyperpro <zhangbjb@126.com>
2020-07-28 22:49:46 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# video_test
+# internet_video
+
+## Install dependency
+    pip install flask
+    pip install youtube_dl
+
+## Run the server
+    python server.py
+  
+
+
+
---
+++ server_george.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from youtube_dl import YoutubeDL
+import time
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+def get_time():
+    return str(hash(str(time.time())))
+
+
+def extract_url(youtube_id):
+    ydl = YoutubeDL()
+    ydl.add_default_info_extractors()
+    weburl = 'https://www.youtube.com/watch?v=' + youtube_id
+    info = ydl.extract_info(weburl, download=False)
+    resolution = {
+        '133': '240',
+        '134': '360',
+        '135': '480',
+        '136': '720',
+        '137': '1080',
+        '160': '144',
+        '278': '144',
+        '242': '240',
+        '243': '360',
+        '244': '480',
+        '247': '720',
+        '248': '1080'
+    }
+    video_type = {
+        '133': 'video/mp4', '134': 'video/mp4', '135': 'video/mp4', '136': 'video/mp4',
+        '137': 'video/mp4', '18': 'video/mp4', '22': 'video/mp4',
+        '160': 'video/mp4',
+        '278': 'video/webm', '242': 'video/webm', '243': 'video/webm', '244': 'video/webm',
+        '247': 'video/webm', '248': 'video/webm'
+    }
+    audio_list = ['139', '140', '141', '256', '258', '325', '328', '171', '172', '249', '250', '251']
+    audio_type = {
+        '139': 'audio/m4a', '140': 'audio/m4a', '141': 'audio/m4a', '256': 'audio/m4a',
+        '258': 'audio/m4a', '325': 'audio/m4a', '328': 'audio/m4a',
+        '171': 'audio/webm', '172': 'audio/webm', '249': 'audio/webm',
+        '250': 'audio/webm', '251': 'audio/webm'
+    }
+
+    quote = "\""
+    first_video = True
+    first_audio = True
+    gen_resolution = set()
+
+    return_video_source = ''
+
+    for x in info['formats']:
+        idx = x['format_id']
+        if (idx in audio_list) and first_audio:
+            first_audio = False
+            return_video_source += "<audio preload id=\"audio\">"
+            return_video_source = return_video_source + "<source src=" + quote + x[
+                'url'] + quote + ' ' + "type=" + quote + audio_type[idx] + quote + ' ' + "/>" + '\n'
+            return_video_source += "</audio>\n"
+
+        if (idx in resolution) and (resolution[idx] not in gen_resolution):
+            gen_resolution.add(resolution[idx])
+            if not first_video:
+                return_video_source = return_video_source + "<source src=" + quote + x[
+                    'url'] + quote + ' ' + "type=" + quote + video_type[idx] + quote + ' ' + "label=" + quote + \
+                                      resolution[idx] + "p" + quote + ' ' + "res=" + quote + resolution[
+                                          idx] + quote + ' ' + "/>"
+            else:
+                first_video = False
+                return_video_source = return_video_source + "<source src=" + quote + x[
+                    'url'] + quote + ' ' + "type=" + quote + video_type[idx] + quote + ' ' + "label=" + quote + \
+                                      resolution[idx] + "p" + quote + ' ' + "res=" + quote + resolution[
+                                          idx] + quote + ' ' + "default label=" + quote + resolution[
+                                          idx] + "p" + quote + ' ' + '/>\n'
+    return return_video_source
+
+
+@app.route('/play/<string:page_name>/')
+def render_video(page_name):
+    video_source = extract_url(page_name)
+    return render_template('video_player_youtube.html', videosource=video_source)
+
+@app.route('/graphs')
+def render_graph():
+    return render_template('graphs.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(debug=True, port=5000)
+    # app.run(debug=False, host='silver.cs.uchicago.edu', port=5000)
